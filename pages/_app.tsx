4ba0d5456c380ee547a62da985c981f205722670
Tanmay <shahhtanmay@gmail.com>
2023-11-30 19:18:25 +0530

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/global.css";
 import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <GoogleAnalytics trackPageViews strategy="lazyOnload" />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp;
